57d073903812975253ae1195355a892bc4f17a1c
Jeremy Parris <jeremy.parris@a-f-p.com>
2018-11-06 14:08:26 -0600

    download brochure

---
+++ Centex/Controllers/api/DownloadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Centex.Controllers.api
+{
+    public class DownloadController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Download()
+        {
+            HttpResponseMessage result = null;
+            var localPath = HttpContext.Current.Server.MapPath("~/Brochures/DemagProBrochure.pdf");
+
+            if(!File.Exists(localPath))
+            {
+                result = Request.CreateResponse(HttpStatusCode.Gone);
+            } else
+            {
+                result = Request.CreateResponse(HttpStatusCode.OK);
+                result.Content = new StreamContent(new FileStream(localPath, FileMode.Open, FileAccess.Read));
+                result.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                result.Content.Headers.ContentDisposition.FileName = "DemagProBrochure.pdf";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Centex/app/Downloads/downloadcontroller.js
@@ -0,0 +1,5 @@
+﻿app.controller('DownloadController', function ($scope) {
+    $scope.download = function () {
+        window.open('/api/download', '_blank', '');
+    };
+});
